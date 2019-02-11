56afa67eacff937214217440054e379dd10689b1
anmolkabra <ak2426@cornell.edu>
2019-02-10 21:58:03 -0500

    Make flex compatible with cup Symbol

--- src/main/java/lexer/XiToken.java
+++ src/main/java/lexer/XiToken.java
@@ -26,7 +26,7 @@ enum TokenType {
     LENGTH,
 
     // operators
-    EQ, //=
+    EQ,//=
     MINUS,
     PLUS,
     NOT,
@@ -53,63 +53,28 @@ enum TokenType {
     RBRAC,
     LCURL,
     RCURL,
-    ERROR
+    ERROR,
+
+    EOF
 }
 
-public class XiToken extends Symbol{
+public class XiToken extends Symbol {
 
     private TokenType type;
     private int line;
     private int col;
     private Object value;
 
-    XiToken(TokenType type, int line, int col, Object value) {
-        super(type.ordinal(), -1, -1, value);
-        this.type = type;
-        this.line = line;
-        this.col = col;
-        this.value = value;
-    }
-
     XiToken(TokenType type, int line, int col, int left, int right, Object value) {
         super(type.ordinal(), left, right, value);
         this.type = type;
-        this.line = line;
-        this.col = col;
+        this.line = line + 1; // flex outputs 0-indexed line, we need 1-indexed
+        this.col = col + 1;
         this.value = value;
     }
 
-    private String format() {
-        String s = value.toString();
-        switch (type) {
-            case STRING_LIT:
-            case CHAR_LIT:
-                return s.replace("\\", "\\\\")
-                        .replace("\n", "\\n")
-                        .replace("\r", "\\r")
-                        .replace("\t", "\\t")
-                        .replace("\"", "\\\"")
-                        .replace("\'", "\\'");
-            default: return s;
-        }
-    }
-
-    public String toString() {
-        String type_rep = "";
-        switch (type) {
-            case INT_LIT:       type_rep = "integer "; break;
-            case STRING_LIT:    type_rep = "string "; break;
-            case CHAR_LIT:      type_rep = "character "; break;
-            case ID:            type_rep = "id "; break;
-            case ERROR:         type_rep = "error:"; break;
-            default:            break;
-        }
-        // make line and col 1-indexed
-        return (line+1) + ":" + (col+1) + " " + type_rep + format();
-    }
-
-    public boolean isError() {
-        return type == TokenType.ERROR;
+    XiToken(TokenType type, int line, int col, Object value) {
+        this(type, line, col, -1, -1, value);
     }
 
     public TokenType getType() {
@@ -126,5 +91,37 @@ public class XiToken extends Symbol{
 
     public Object getValue() {
         return value;
+    }
+
+    public boolean isError() {
+        return getType() == TokenType.ERROR;
+    }
+
+    private String format() {
+        String s = getValue().toString();
+        switch (getType()) {
+            case STRING_LIT:
+            case CHAR_LIT:
+                return s.replace("\\", "\\\\")
+                        .replace("\n", "\\n")
+                        .replace("\r", "\\r")
+                        .replace("\t", "\\t")
+                        .replace("\"", "\\\"")
+                        .replace("\'", "\\'");
+            default: return s;
+        }
+    }
+
+    public String toString() {
+        String type_rep = "";
+        switch (getType()) {
+            case INT_LIT:       type_rep = "integer "; break;
+            case STRING_LIT:    type_rep = "string "; break;
+            case CHAR_LIT:      type_rep = "character "; break;
+            case ID:            type_rep = "id "; break;
+            case ERROR:         type_rep = "error:"; break;
+            default:            break;
+        }
+        return getLine() + ":" + getCol() + " " + type_rep + format();
     }
 }
