919e20de8073e9e701a64d20f29584602780cbf7
Kirill Chernyshov <kc875@cornell.edu>
2019-02-11 13:32:38 -0500

    Tidy up imports

--- src/main/java/xi_parser/AstNode.java
+++ src/main/java/xi_parser/AstNode.java
@@ -1,7 +1,10 @@
 package xi_parser;
 
-import java.util.*;
 import polyglot.util.Pair;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 enum ExprType {
     BinopExpr,
