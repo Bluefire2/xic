d95d35c3d6dde21a58d55c1d15afb72e190fddd7
Kirill Chernyshov <kc875@cornell.edu>
2019-02-15 18:05:34 -0500

    Add docstring for lookup

--- src/main/java/symboltable/SymbolTable.java
+++ src/main/java/symboltable/SymbolTable.java
@@ -3,5 +3,13 @@ package symboltable;
 import ast.Type;
 
 public interface SymbolTable {
+    /**
+     * Look up the type of an identifier.
+     *
+     * @param ID The identifier.
+     * @return The type of the identifier, if it has been declared.
+     * @throws NotFoundException If there is no identifier {@code ID} in the
+     *         current context.
+     */
     Type lookup(String ID) throws NotFoundException;
 }
