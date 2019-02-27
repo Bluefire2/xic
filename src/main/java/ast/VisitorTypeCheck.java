436bda3b02ff1ef3276b293dc982d050abdc1c0a
anmolkabra <ak2426@cornell.edu>
2019-02-26 19:47:36 -0500

    Fix non-unit funcdefs explicitly returning issue, add empty interface test

--- src/main/java/ast/VisitorTypeCheck.java
+++ src/main/java/ast/VisitorTypeCheck.java
@@ -688,7 +688,13 @@ public class VisitorTypeCheck implements VisitorAST {
                 symTable.add(param.part1(), new TypeSymTableVar(param.part2()));
             }
         }
-        node.getBody().accept(this);
+        Stmt body = node.getBody();
+        body.accept(this);
+        if (!(node.getOutput() instanceof TypeTUnit)
+                && body.typeCheckType != TypeR.Void)
+            // func def returns non-unit but doesn't end with a return
+            throw new SemanticError("Missing return",
+                    body.getLocation());
         symTable.exitScope();
     }
 
