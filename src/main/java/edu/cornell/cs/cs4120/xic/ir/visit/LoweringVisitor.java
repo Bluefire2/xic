9462ccc17e8255aa8d1c61a95ed84e6fc8c718a9
cassandrascarpa <cls354@cornell.edu>
2019-03-05 12:48:57 -0500

    add BasicBlock class

--- src/main/java/edu/cornell/cs/cs4120/xic/ir/visit/LoweringVisitor.java
+++ src/main/java/edu/cornell/cs/cs4120/xic/ir/visit/LoweringVisitor.java
@@ -2,10 +2,58 @@ package edu.cornell.cs.cs4120.xic.ir.visit;
 
 import edu.cornell.cs.cs4120.xic.ir.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LoweringVisitor extends IRVisitor {
+
+    private class BasicBlock {
+        List<IRNode> statements;
+        BasicBlock next;
+
+        BasicBlock () {
+            statements = new ArrayList<>();
+            next = null;
+        }
+
+        void addStmt(IRNode stmt) {
+            statements.add(stmt);
+        }
+
+        BasicBlock getNext() {
+            return next;
+        }
+
+        void setNext(BasicBlock next) {
+            this.next = next;
+        }
+    }
+
+    BasicBlock head;
+    BasicBlock current;
 
     public LoweringVisitor(IRNodeFactory inf) {
         super(inf);
+        head = new BasicBlock();
+        current = head;
+    }
+
+    private void addNodeToBlock(IRNode node) {
+        if(node instanceof IRLabel && current != head) {
+            BasicBlock newblock = new BasicBlock();
+            newblock.addStmt(node);
+            current.setNext(newblock);
+            current = newblock;
+        }
+        else if (node instanceof IRReturn || node instanceof IRJump || node instanceof IRCJump) {
+            current.addStmt(node);
+            BasicBlock newblock = new BasicBlock();
+            current.setNext(newblock);
+            current = newblock;
+        }
+        else {
+            current.addStmt(node);
+        }
     }
 
     private boolean ifExprsCommute(IRExpr e1, IRExpr e2) {
