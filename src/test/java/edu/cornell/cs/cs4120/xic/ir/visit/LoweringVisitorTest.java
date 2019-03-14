bd6fb744871360c484459adc5a5788e9b3b784b2
Kirill Chernyshov <kc875@cornell.edu>
2019-03-14 00:15:49 -0400

    imports

--- src/test/java/edu/cornell/cs/cs4120/xic/ir/visit/LoweringVisitorTest.java
+++ src/test/java/edu/cornell/cs/cs4120/xic/ir/visit/LoweringVisitorTest.java
@@ -6,7 +6,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class LoweringVisitorTest {
     private LoweringVisitor visitor;
