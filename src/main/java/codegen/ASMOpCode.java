6495bb8068728882700a697259e31a1885448087
cassandrascarpa <cls354@cornell.edu>
2019-03-23 21:11:37 -0400

    Add opcode enum

--- src/main/java/codegen/ASMOpCode.java
+++ src/main/java/codegen/ASMOpCode.java
@@ -1,5 +1,95 @@
 package codegen;
 
 public enum ASMOpCode {
-    //TODO
+
+    //Arithmetic
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    IMUL,
+    IDIV,
+    NEG,
+    ADDC,
+    SBB,
+    INC,
+    DEC,
+
+    //Logical
+    AND,
+    OR,
+    XOR,
+    NOT,
+
+    //Shift and rotate
+    SHR,
+    SHL,
+    SAR,
+    SAL,
+    SHLD,
+    SHRD,
+    ROR,
+    ROL,
+    RCR,
+    RCL,
+
+    //Data transfer
+    MOV,
+    XCHG,
+    CMPXCHG,
+    MOVZ,
+    MOVZX,
+    MOVS,
+    MOVSX,
+    MOVSB,
+    MOVSW,
+    LEA,
+    LDS,
+    PUSH,
+    POP,
+    PUSHA,
+    POPA,
+    PUSHF,
+    POPF,
+    IN,
+    OUT,
+
+    //Control flow
+    TEST,
+    CMP,
+    CALL,
+    RET,
+
+    JMP,
+    JE,
+    JNE,
+    JG,
+    JGE,
+    JA,
+    JAE,
+    JL,
+    JLE,
+    JB,
+    JBE,
+    JO,
+    JNO,
+    JZ,
+    JNZ,
+    JS,
+    JNS,
+
+    LOOP,
+    LOOPE,
+    LOOPNE,
+    LOOPZ,
+    LOOPNZ,
+
+    ENTER,
+    LEAVE,
+
+    HLT,
+    LOCK,
+    NOP,
+    WAIT
+
 }
