392ec59bf3eac264c7ab618c9d6797d622cb10d3
anmolkabra <ak2426@cornell.edu>
2019-03-30 23:13:29 -0400

    Remove reg enum, implement logical binop translation

--- src/main/java/asm/ASMExprReg.java
+++ src/main/java/asm/ASMExprReg.java
@@ -1,38 +1,18 @@
 package asm;
 
 public class ASMExprReg extends ASMExpr {
-    public enum x86_64Reg {
-        rax,
-        rbx,
-        rcx,
-        rdx,
-        rsi,
-        rdi,
-        rbp,
-        rsp,
-        r8,
-        r9,
-        r10,
-        r11,
-        r12,
-        r13,
-        r14,
-        r15,
-        rip;
-    }
+    private String reg;
 
-    private x86_64Reg reg;
-
-    public ASMExprReg(x86_64Reg reg) {
+    public ASMExprReg(String reg) {
         this.reg = reg;
     }
 
-    public x86_64Reg getReg() {
+    public String getReg() {
         return reg;
     }
 
     @Override
     public String toString() {
-        return reg.toString();
+        return reg;
     }
 }
