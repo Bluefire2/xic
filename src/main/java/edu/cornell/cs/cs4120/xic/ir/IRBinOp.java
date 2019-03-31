f2ed4139fb7b785d6434a2bd9210d2c38c3880d3
Kirill Chernyshov <kc875@cornell.edu>
2019-03-30 20:55:16 -0400

    This is already the default behaviour

--- src/main/java/edu/cornell/cs/cs4120/xic/ir/IRBinOp.java
+++ src/main/java/edu/cornell/cs/cs4120/xic/ir/IRBinOp.java
@@ -23,51 +23,8 @@ public class IRBinOp extends IRExpr_c {
      */
     public enum OpType {
         ADD, SUB, MUL, HMUL, DIV, MOD, AND, OR, XOR, LSHIFT, RSHIFT, ARSHIFT,
-        EQ, NEQ, LT, GT, LEQ, GEQ;
-
-        @Override
-        public String toString() {
-            switch (this) {
-            case ADD:
-                return "ADD";
-            case SUB:
-                return "SUB";
-            case MUL:
-                return "MUL";
-            case HMUL:
-                return "HMUL";
-            case DIV:
-                return "DIV";
-            case MOD:
-                return "MOD";
-            case AND:
-                return "AND";
-            case OR:
-                return "OR";
-            case XOR:
-                return "XOR";
-            case LSHIFT:
-                return "LSHIFT";
-            case RSHIFT:
-                return "RSHIFT";
-            case ARSHIFT:
-                return "ARSHIFT";
-            case EQ:
-                return "EQ";
-            case NEQ:
-                return "NEQ";
-            case LT:
-                return "LT";
-            case GT:
-                return "GT";
-            case LEQ:
-                return "LEQ";
-            case GEQ:
-                return "GEQ";
-            }
-            throw new InternalCompilerError("Unknown op type");
-        }
-    };
+        EQ, NEQ, LT, GT, LEQ, GEQ
+    }
 
     private OpType type;
     private IRExpr left, right;
