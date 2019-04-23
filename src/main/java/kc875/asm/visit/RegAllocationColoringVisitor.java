61bb3a5a5de1deb1187a515d7ef9c17bba4c0eaa
yangdanny97 <dzy4@cornell.edu>
2019-04-23 16:15:33 -0400

    update coloring

--- src/main/java/kc875/asm/visit/RegAllocationColoringVisitor.java
+++ src/main/java/kc875/asm/visit/RegAllocationColoringVisitor.java
@@ -8,7 +8,6 @@ import kc875.asm.dfa.LiveVariableDFA;
 import kc875.cfg.Graph;
 import com.google.common.collect.Sets;
 import polyglot.util.Pair;
-
 import java.util.*;
 
 public class RegAllocationColoringVisitor {
@@ -22,7 +21,8 @@ public class RegAllocationColoringVisitor {
     HashSet<Graph.Node> coalescedNodes;//registers which are coalesced
     //u <- v coalesced then v is added to this set, u is moved to work list
     HashSet<Graph.Node> coloredNodes; //successfully colored
-    HashSet<Graph.Node> selectStack; //stacks w/ temps removed from graph
+    //TODO maybe we need a stack for this...
+    Stack<Graph.Node> selectStack; //stacks w/ temps removed from graph
 
     //every move is in exactly one of these sets
     HashSet<ASMInstr> coalescedMoves; //coalesced moves
@@ -35,7 +35,10 @@ public class RegAllocationColoringVisitor {
     HashSet<Pair<Graph.Node, Graph.Node>> adjSet;
 
     //list repr of the graph, node -> list of interfering nodes
-    HashMap<Graph.Node, List<Graph.Node>> adjList;
+    HashMap<Graph.Node, Set<Graph.Node>> adjList;
+
+    //Degree of each node
+    HashMap<Graph.Node, Integer> degree;
 
     //map from node -> list of moves it is associated with
     HashMap<Graph.Node, HashSet<ASMInstr>> moveList;
@@ -50,6 +53,8 @@ public class RegAllocationColoringVisitor {
     InterferenceGraph interference;
     LiveVariableDFA liveness;
     List<ASMInstr> instrs;
+
+    int K = 16;//num regs I think
 
     RegAllocationColoringVisitor(){
     }
@@ -68,7 +73,7 @@ public class RegAllocationColoringVisitor {
         spilledNodes = new HashSet<>();
         coalescedNodes = new HashSet<>();
         coloredNodes = new HashSet<>();
-        selectStack = new HashSet<>();
+        selectStack = new Stack<>();
 
         coalescedMoves = new HashSet<>();
         constrainedMoves = new HashSet<>();
@@ -111,34 +116,72 @@ public class RegAllocationColoringVisitor {
     }
 
     public void build(){
+        //TODO initialize interference graph
+        //foralll blocks b in program
         for (Graph<ASMInstr>.Node b : cfg.getAllNodes()){
+            //live = liveout(b)
             Map<Graph.Node, Set<ASMExprRegReplaceable>> outs = liveness.getOutMap();
             Set<ASMExprRegReplaceable> live = outs.get(b);
+            //forall instructions in b in reverse order (1 instruction in b for us)
             ASMInstr i = b.getT();
+            //if ismoveinstr(i)
             if (i.hasNewDef()){
+                //live = live\use(i)
                 live = Sets.difference(live, liveness.use(b));
+                //forall n in def(i)+use(i)
                 for (ASMExprRegReplaceable n : Sets.union(liveness.def(b), liveness.use(b)).immutableCopy()){
-                    moveList.get(n).add(i); //movelist[n] <- movelist[n] + {I}
+                    //movelist[n] <- movelist[n] + {I}
+                    moveList.get(n).add(i);
                 }
+                //worklistMoves = workListMoves + {I}
                 worklistMoves.add(i);
             }
+            //live = live + def(i)
             live = Sets.union(live, liveness.def(b)).immutableCopy();
+            //forall d in def(i)
             for (ASMExprRegReplaceable d : liveness.def(b)){
+                //forall l in live
                 for (ASMExprRegReplaceable l : live) {
-                    //interference.addEdge(interference.tnode(l), interference.tnode(d));
-                    //TODO
+                    //addEdge(l,d)
+                    addEdge(l,d);
                 }
             }
+            //live = use(i)+ (live\def(i)
             live = Sets.union(liveness.use(b), Sets.difference(live, liveness.def(b)).immutableCopy()).immutableCopy();
+            //TODO what do we do with live? store it back into liveness outmap?
         }
     }
 
     public void makeWorkList(){
-        //TODO
+        //forall n in initial
+        for (Graph.Node n : initial){
+            //initial = initial\{n}
+            initial.remove(n);
+            //if degree[n] >= K
+            if (degree.get(n) >= K){
+                //spillWorklist = spillWorklist + {n}
+                spillWorklist.add(n);
+            } else if (isMoveRelated(n)){
+                //freezeWorklist = freezeWorklist + {n}
+                freezeWorklist.add(n);
+            } else {
+                //simplifyWorklist = simplifyWorklist + {n}
+                simplifyWorklist.add(n);
+            }
+        }
     }
 
     public void simplify(){
-        //TODO
+        //TODO selecting things from worklist
+        //let n be in simplifyWorklist
+        Graph.Node n = new ArrayList<>(simplifyWorklist).get(0);
+        //simplifyWorklist = simplifyWorklist \ {n}
+        simplifyWorklist.remove(n);
+        //push n to selectStack
+        selectStack.push(n);
+        for (Graph.Node m : getAdjacent(n)){
+            decrementDegree(m);
+        }
     }
 
     public void coalesce(){
@@ -162,4 +205,98 @@ public class RegAllocationColoringVisitor {
         return null;
     }
 
+    //HELPERS DOWN HERE
+    public void addEdge(ASMExprRegReplaceable u, ASMExprRegReplaceable v){
+        //if (u,v) not in adjSet and u != v
+        Graph.Node uNode = interference.tnode(u);
+        Graph.Node vNode = interference.tnode(v);
+        if (!adjSet.contains(new Pair<>(uNode,vNode)) && !u.equals(v)){//TODO does contains work here
+            //adjSet = adjSet + {(u,v), (v,u)}
+            adjSet.add(new Pair<>(uNode,vNode));
+            adjSet.add(new Pair<>(vNode,uNode));
+        }
+        //if u not in precollored
+        if (!precolored.contains(uNode)){
+            //adjList[u] = adjList[u] + {v}
+            adjList.get(uNode).add(vNode);
+            //degree[u] = degree[u] + 1
+            degree.put(uNode, degree.get(uNode) + 1);
+        }
+        //same for v
+        if (!precolored.contains(vNode)){
+            adjList.get(vNode).add(uNode);
+            degree.put(vNode, degree.get(vNode) + 1);
+        }
+    }
+
+    public Set<Graph.Node> getAdjacent(Graph.Node n){
+        //adjList[n]\(activeMoves + coalescedNodes)
+        return Sets.difference(
+                adjList.get(n),
+                Sets.union(new HashSet<>(selectStack), coalescedNodes).immutableCopy()
+        ).immutableCopy();
+    }
+
+    public Set<ASMInstr> getNodeMoves(Graph.Node n){
+        //moveList[n] intersection (activeMoves + worklistMoves)
+        return Sets.intersection(
+                moveList.get(n),
+                Sets.union(activeMoves, worklistMoves).immutableCopy()
+        ).immutableCopy();
+    }
+
+    public boolean isMoveRelated(Graph.Node n){
+        return getNodeMoves(n).size() != 0;
+    }
+
+    public void decrementDegree(Graph.Node m){
+        //let d = degree[m]
+        int d = degree.get(m);
+        //degree[m] = d-1
+        degree.put(m, d - 1);
+        //if d == K
+        if (d == K){
+            //enableMoves({m} + adjacent(m))
+            Set<Graph.Node> s = new HashSet<>();
+            s.add(m);
+            enableMoves(Sets.union(s, getAdjacent(m)).immutableCopy());
+            spillWorklist.remove(m);
+        }
+        if (isMoveRelated(m)){
+            freezeWorklist.add(m);
+        } else {
+            simplifyWorklist.add(m);
+        }
+    }
+
+    public void enableMoves(Set<Graph.Node> nodes){
+        for (Graph.Node n : nodes){
+            for (ASMInstr m : activeMoves){
+                activeMoves.remove(m);
+                worklistMoves.add(m);
+            }
+        }
+    }
+
+    public void addWorkList(Graph.Node u){
+        if (!precolored.contains(u) && !(isMoveRelated(u)) && degree.get(u) < K) {
+            freezeWorklist.remove(u);
+            simplifyWorklist.add(u);
+        }
+    }
+
+    public boolean ok(Graph.Node t, Graph.Node r){
+        //TODO is contains ok here?
+        return degree.get(t) < K && precolored.contains(t) && adjSet.contains(new Pair<>(t,r));
+    }
+
+    public boolean conservative(Set<Graph.Node> nodes){
+        int k = 0;
+        for (Graph.Node n : nodes){
+            if (degree.get(n) >= K){
+                k = k + 1;
+            }
+        }
+        return k < K;
+    }
 }
