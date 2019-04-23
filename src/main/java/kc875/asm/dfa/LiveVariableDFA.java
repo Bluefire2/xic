636062e155c3744ec2824db8a148c69b8588c9c7
anmolkabra <ak2426@cornell.edu>
2019-04-22 22:37:14 -0400

    Fix generic DFA Framework graph field

--- src/main/java/kc875/asm/dfa/LiveVariableDFA.java
+++ src/main/java/kc875/asm/dfa/LiveVariableDFA.java
@@ -1,6 +1,7 @@
 package kc875.asm.dfa;
 
 import kc875.asm.ASMExprRegReplaceable;
+import kc875.asm.ASMInstr;
 import kc875.cfg.DFAFramework;
 import kc875.cfg.Graph;
 import kc875.utils.SetWithInf;
@@ -9,11 +10,11 @@ import kc875.utils.SetWithInf;
  * Live variable DFA (used in reg allocation). The lattice elements are sets
  * of Temps/Regs.
  */
-public class LiveVariableDFA extends DFAFramework<SetWithInf<ASMExprRegReplaceable>> {
+public class LiveVariableDFA extends DFAFramework<SetWithInf<ASMExprRegReplaceable>, ASMInstr> {
 
-    public LiveVariableDFA(Graph graph) {
+    public LiveVariableDFA(ASMGraph asmGraph) {
         super(
-                graph,
+                asmGraph,
                 Direction.BACKWARD,
                 (node, l) -> use(node).union(l.diff(def(node))),
                 SetWithInf::union,
