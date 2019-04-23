940010f2beb31652316cc90422ef40b5b72c036c
Kirill Chernyshov <kc875@cornell.edu>
2019-04-22 22:24:42 -0400

    Add first pass for CFG construction

--- src/main/java/kc875/asm/dfa/ASMGraph.java
+++ src/main/java/kc875/asm/dfa/ASMGraph.java
@@ -2,14 +2,20 @@ package kc875.asm.dfa;
 
 import kc875.asm.ASMExprRegReplaceable;
 import kc875.asm.ASMInstr;
+import kc875.asm.ASMInstrLabel;
+import kc875.asm.ASMOpCode;
 import kc875.cfg.Graph;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class ASMGraph extends Graph<ASMInstr> {
     private HashMap<Node, List<ASMExprRegReplaceable>> genMap;
     private HashMap<Node, List<ASMExprRegReplaceable>> killMap;
+    private HashMap<ASMInstrLabel, Node> labels;
+
+    private static Set<ASMOpCode> noFallthrough = new HashSet<>(
+            Arrays.asList(ASMOpCode.JMP, ASMOpCode.RET, ASMOpCode.CALL) // TODO
+    );
 
     public List<ASMExprRegReplaceable> getUse(ASMInstr i) {
         //TODO
@@ -21,17 +27,64 @@ public class ASMGraph extends Graph<ASMInstr> {
         return null;
     }
 
+    /**
+     * Check if an instruction needs a CFG edge to the instruction immediately
+     * following it. This is false if the instruction is non-fallthrough, e.g.
+     * an unconditional jump.
+     *
+     * @param i The instruction.
+     * @return {@code true} if the instruction should have an edge to the next
+     * instruction, {@code false} otherwise.
+     */
+    private boolean edgeToNextNode(ASMInstr i) {
+        return !noFallthrough.contains(i.getOpCode());
+    }
+
     //constructor that returns a new flow graph
     public ASMGraph(List<ASMInstr> instrs) {
         this.genMap = new HashMap<>();
         this.killMap = new HashMap<>();
+        this.labels = new HashMap<>();
 
-        for (ASMInstr ins : instrs) {
-            Node node = new Node(ins);
-            genMap.put(node, getUse(ins));
-            killMap.put(node, getDef(ins));
-            //TODO configure edges using inherited graph methods
+        if (instrs.isEmpty()) return;
+
+        // FIRST PASS: add all instructions as nodes
+
+        // set the start node
+        Iterator<ASMInstr> iter = instrs.iterator();
+        Node start = new Node(iter.next());
+        this.setStartNode(start);
+
+        Node previous = start;
+        // if the previous instr was a label, this is non-null
+        ASMInstrLabel previousLabel = null;
+
+        while (iter.hasNext()) {
+            ASMInstr instr = iter.next();
+            if (instr.getOpCode() != ASMOpCode.LABEL) {
+                Node node = new Node(instr);
+                this.addOtherNode(node);
+
+                // add an edge from the previous instruction's node if we need to
+                if (edgeToNextNode(previous.getT())) {
+                    this.addEdge(previous, node);
+                }
+                previous = node;
+
+                // if the last instruction was a label, this node needs to be pointed to by that label
+                if (previousLabel != null) {
+                    labels.put(previousLabel, node);
+
+                    // make sure to set this back to null
+                    previousLabel = null;
+                }
+            } else {
+                // don't make nodes for labels
+                previousLabel = (ASMInstrLabel) instr;
+            }
         }
+
+        // TODO: second pass, add CFG edges for jumps to labelled nodes
     }
 
     public ASMInstr instr(Node node) {
