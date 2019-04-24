46e281eefc745ae33ba2e07d4e5808b8b75d66ae
cassandrascarpa <cls354@cornell.edu>
2019-04-23 23:57:22 -0400

    Move cfg generation to IRGraph

--- src/main/java/edu/cornell/cs/cs4120/xic/ir/dfa/IRGraph.java
+++ src/main/java/edu/cornell/cs/cs4120/xic/ir/dfa/IRGraph.java
@@ -1,32 +1,63 @@
 package edu.cornell.cs.cs4120.xic.ir.dfa;
 
-import edu.cornell.cs.cs4120.xic.ir.IRExpr;
-import edu.cornell.cs.cs4120.xic.ir.IRStmt;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import edu.cornell.cs.cs4120.xic.ir.*;
 import kc875.cfg.Graph;
-
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class IRGraph extends Graph<IRStmt> {
 
-    private HashMap<Node, List<IRExpr>> genMap;
-    private HashMap<Node, List<IRExpr>> killMap;
+    private BiMap<Node, IRStmt> nodeMap;
 
-    public IRGraph(List<IRStmt> quadruples){
-        //TODO: complete, change from quadruple to IRStmt
-        for (IRStmt q : quadruples) {
-            Graph.Node n = new Graph.Node(q);
-            addOtherNode(n);
-            genMap.put(n, getGen(n));
-            killMap.put(n, getKill(n));
+    public IRGraph(List<IRStmt> stmts){
+        nodeMap = HashBiMap.create();
+
+        List<IRStmt> basicBlocks = new ArrayList<>();
+        HashMap<String, Integer> nodeLabelMap = new HashMap<>();
+        HashMap<Integer, List<String>> jumps = new HashMap<>();
+
+        for (IRStmt q : stmts) {
+
+            IRSeq curr = new IRSeq();
+
+            for (IRStmt s : stmts) {
+                if (s instanceof IRLabel) {
+                    basicBlocks.add(curr);
+                    curr = new IRSeq();
+                    curr.stmts().add(s);
+                    nodeLabelMap.put(((IRLabel)s).name(), basicBlocks.size());
+                }
+                else curr.stmts().add(s);
+                if (s instanceof IRJump ||
+                        s instanceof IRCJump ||
+                        s instanceof IRReturn) {
+                    basicBlocks.add(curr);
+                    curr = new IRSeq();
+                    List<String> blockjumps = new ArrayList<>();
+                    if (s instanceof IRCJump) {
+                        IRCJump sj = (IRCJump) s;
+                        blockjumps.add(sj.trueLabel());
+                        blockjumps.add(sj.falseLabel());
+                    }
+                    jumps.put(basicBlocks.size(), blockjumps);
+                }
+            }
         }
+
+        for (IRStmt bb : basicBlocks) {
+            Graph.Node n = new Graph.Node(bb);
+            if (getStartNode() == null) {
+                setStartNode(n);
+            }
+            else addOtherNode(n);
+        }
+
+        //TODO: add edges
+
     }
 
-    public List<IRExpr> getGen(Node n) {
-        return genMap.get(n);
-    }
 
-    public List<IRExpr> getKill(Node n) {
-        return killMap.get(n);
-    }
 }
--- src/main/java/edu/cornell/cs/cs4120/xic/ir/visit/CommonSubexprElimVisitor.java
+++ src/main/java/edu/cornell/cs/cs4120/xic/ir/visit/CommonSubexprElimVisitor.java
@@ -3,8 +3,6 @@ package edu.cornell.cs.cs4120.xic.ir.visit;
 import edu.cornell.cs.cs4120.xic.ir.*;
 import edu.cornell.cs.cs4120.xic.ir.dfa.AvailableExprsDFA;
 import edu.cornell.cs.cs4120.xic.ir.dfa.IRGraph;
-import kc875.cfg.Graph;
-import kc875.utils.SetWithInf;
 
 import java.util.*;
 
@@ -25,41 +23,7 @@ public class CommonSubexprElimVisitor {
         }
         else stmts.add(topstmt);
 
-        List<IRStmt> basicBlocks = new ArrayList<>();
-        HashMap<String, Integer> nodeLabelMap = new HashMap<>();
-        HashMap<Integer, List<String>> jumps = new HashMap<>();
-
-        IRLabel start = new IRLabel(irnode.name());
-
-        IRSeq curr = new IRSeq();
-
-        for (IRStmt s : stmts) {
-            if (s instanceof IRLabel) {
-                basicBlocks.add(curr);
-                curr = new IRSeq();
-                curr.stmts().add(s);
-                nodeLabelMap.put(((IRLabel)s).name(), basicBlocks.size());
-            }
-            else curr.stmts().add(s);
-            if (s instanceof IRJump ||
-                    s instanceof IRCJump ||
-                    s instanceof IRReturn) {
-                basicBlocks.add(curr);
-                curr = new IRSeq();
-                List<String> blockjumps = new ArrayList<>();
-                if (s instanceof IRCJump) {
-                    IRCJump sj = (IRCJump) s;
-                    blockjumps.add(sj.trueLabel());
-                    blockjumps.add(sj.falseLabel());
-                }
-                jumps.put(basicBlocks.size(), blockjumps);
-            }
-        }
-
-        IRGraph irGraph = new IRGraph(basicBlocks);
-        //TODO: add edges
-
-        return irGraph;
+        return new IRGraph(stmts);
     }
 
     public IRCompUnit removeCommonSubExpressions(IRCompUnit irnode) {
