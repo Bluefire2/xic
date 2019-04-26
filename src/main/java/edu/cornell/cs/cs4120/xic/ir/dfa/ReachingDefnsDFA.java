b546c1b21d7a6dec94a00035d59ebf7b6f38eb59
cassandrascarpa <cls354@cornell.edu>
2019-04-26 14:16:58 -0400

    Reaching definitions analysis for IR

--- src/main/java/edu/cornell/cs/cs4120/xic/ir/dfa/ReachingDefnsDFA.java
+++ src/main/java/edu/cornell/cs/cs4120/xic/ir/dfa/ReachingDefnsDFA.java
@@ -1,40 +1,66 @@
 package edu.cornell.cs.cs4120.xic.ir.dfa;
 
 import com.google.common.collect.Sets;
-import edu.cornell.cs.cs4120.xic.ir.IRStmt;
-import edu.cornell.cs.cs4120.xic.ir.IRTemp;
+import edu.cornell.cs.cs4120.xic.ir.*;
 import kc875.cfg.DFAFramework;
+import kc875.cfg.Graph;
+
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-public class ReachingDefnsDFA extends DFAFramework<Set<IRTemp>, IRStmt> {
+public class ReachingDefnsDFA extends DFAFramework<Set<IRGraph.Node>, IRStmt> {
 
     public ReachingDefnsDFA(IRGraph graph) {
         super(
                 graph,
                 DFAFramework.Direction.FORWARD,
                 (node, l) -> new HashSet<>(Sets.union(gen(node),
-                        Sets.difference(l, kill(node)))),
+                        Sets.difference(l, kill(node, graph)))),
                 HashSet::new,
-                (l1, l2) -> new HashSet<>(Sets.union(l1, l2)),
+                (l1, l2) -> new HashSet<IRGraph.Node>(Sets.union(l1, l2)),
                 new HashSet<>()
         );
     }
 
-    public static Set<IRTemp> gen(IRGraph.Node node) {
-        //TODO
-        return new HashSet<>();
-
+    public static Set<IRGraph.Node> gen(IRGraph.Node node) {
+        HashSet<IRGraph.Node> genSet = new HashSet<>();
+        IRStmt s = node.getT();
+        if (s instanceof IRMove) {
+            if (((IRMove) s).target() instanceof IRTemp) genSet.add(node);
+        }
+        return genSet;
     }
 
-    public static Set<IRTemp> kill(IRGraph.Node node) {
-        //TODO
+    public static Set<IRGraph.Node> kill(IRGraph.Node node, IRGraph graph) {
+        IRStmt s = node.getT();
+        if (s instanceof IRMove) {
+            IRExpr target = ((IRMove) s).target();
+            if (target instanceof IRTemp) return defs((IRTemp) target, graph);
+        }
         return new HashSet<>();
     }
 
-    public static Set<IRGraph.Node> defs(IRTemp tmp) {
-        //TODO
-        return new HashSet<>();
+    /**
+     * Return the set of all nodes that define a given variable.
+     * @param x
+     * @param graph
+     * @return Set of nodes in graph that define x.
+     */
+    public static Set<IRGraph.Node> defs(IRTemp x, IRGraph graph) {
+        Set<IRGraph.Node> defSet = new HashSet<>();
+        for (IRGraph.Node n : graph.getAllNodes()) {
+            IRStmt s = n.getT();
+            if (s instanceof IRMove) {
+                IRExpr target = ((IRMove) s).target();
+                if (target instanceof IRTemp &&
+                        ((IRTemp) target).name().equals(x.name())) {
+                    defSet.add(n);
+                }
+            }
+        }
+        return defSet;
     }
 
 }
