c395af827f08166bd953aca1b4f7892ebc7c38dc
cassandrascarpa <cls354@cornell.edu>
2019-04-27 19:47:55 -0400

    Refactor code to build IR graph

--- src/main/java/edu/cornell/cs/cs4120/xic/ir/dfa/IRGraph.java
+++ src/main/java/edu/cornell/cs/cs4120/xic/ir/dfa/IRGraph.java
@@ -65,6 +65,39 @@ public class IRGraph extends Graph<IRStmt> {
 
     }
 
+    /**
+     * Build the per-function control graph.
+     * @param irnode The root IR node of the function declaration
+     * @return an IRGraph, with basic blocks as nodes and jumps as edges.
+     */
+    public static IRGraph buildCFG(IRFuncDecl irnode) {
+        List<IRStmt> stmts = new ArrayList<>();
+        IRStmt topstmt = irnode.body();
+        if (topstmt instanceof IRSeq) {
+            stmts.addAll(((IRSeq) topstmt).stmts());
+        }
+        else stmts.add(topstmt);
+
+        return new IRGraph(stmts);
+    }
+
+    /**
+     * Flatten control flow graph back into IR
+     * @param irGraph
+     * @return an IR statement (IRSeq) constructed from irGraph
+     */
+    public static IRStmt flattenCFG(IRGraph irGraph) {
+        IRSeq retseq = new IRSeq();
+        for (Graph.Node n : irGraph.getAllNodes()) {
+            IRStmt s = irGraph.getStmt(n);
+            if (s instanceof IRSeq) {
+                retseq.stmts().addAll(((IRSeq) s).stmts());
+            }
+            else retseq.stmts().add(s);
+        }
+        return retseq;
+    }
+
     public IRStmt getStmt(Node n) { return nodeMap.get(n); }
 
     public void setStmt(Node n, IRStmt s) { nodeMap.replace(n,s); }
--- src/main/java/edu/cornell/cs/cs4120/xic/ir/visit/CommonSubexprElimVisitor.java
+++ src/main/java/edu/cornell/cs/cs4120/xic/ir/visit/CommonSubexprElimVisitor.java
@@ -18,39 +18,6 @@ public class CommonSubexprElimVisitor {
         return String.format("cse_t%d", tempcounter++);
     }
 
-    /**
-     * Build the per-function control graph.
-     * @param irnode The root IR node of the function declaration
-     * @return an IRGraph, with basic blocks as nodes and jumps as edges.
-     */
-    public IRGraph buildCFG(IRFuncDecl irnode) {
-        List<IRStmt> stmts = new ArrayList<>();
-        IRStmt topstmt = irnode.body();
-        if (topstmt instanceof IRSeq) {
-            stmts.addAll(((IRSeq) topstmt).stmts());
-        }
-        else stmts.add(topstmt);
-
-        return new IRGraph(stmts);
-    }
-
-    /**
-     * Flatten control flow graph back into IR
-     * @param irGraph
-     * @return an IR statement (IRSeq) constructed from irGraph
-     */
-    public IRStmt flattenCFG(IRGraph irGraph) {
-        IRSeq retseq = new IRSeq();
-        for (Graph.Node n : irGraph.getAllNodes()) {
-            IRStmt s = irGraph.getStmt(n);
-            if (s instanceof IRSeq) {
-                retseq.stmts().addAll(((IRSeq) s).stmts());
-            }
-            else retseq.stmts().add(s);
-        }
-        return retseq;
-    }
-
 
     /**
      * Perform common subexpression elimination
@@ -60,7 +27,7 @@ public class CommonSubexprElimVisitor {
     public IRCompUnit removeCommonSubExpressions(IRCompUnit irnode) {
         IRCompUnit optimizedCompUnit = new IRCompUnit(irnode.name());
         for (IRFuncDecl funcDecl : irnode.functions().values()) {
-            irGraph = buildCFG(funcDecl);
+            irGraph = IRGraph.buildCFG(funcDecl);
             AvailableExprsDFA availableExprsDFA = new AvailableExprsDFA(irGraph);
             availableExprsDFA.runWorklistAlgo();
 
@@ -83,7 +50,7 @@ public class CommonSubexprElimVisitor {
             }
 
             IRFuncDecl optimizedFuncDecl = new IRFuncDecl(funcDecl.name(),
-                    flattenCFG((IRGraph) availableExprsDFA.getGraph()));
+                    IRGraph.flattenCFG((IRGraph) availableExprsDFA.getGraph()));
             optimizedCompUnit.functions().put(funcDecl.name(), optimizedFuncDecl);
         }
         return optimizedCompUnit;
