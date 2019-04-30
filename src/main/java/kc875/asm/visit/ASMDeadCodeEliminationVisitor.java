befe8368dde2e48dbc5579940e757063c5475ff8
anmolkabra <ak2426@cornell.edu>
2019-04-30 16:58:17 -0400

    Forgot the main file

---
+++ src/main/java/kc875/asm/visit/ASMDeadCodeEliminationVisitor.java
@@ -0,0 +1,50 @@
+package kc875.asm.visit;
+
+import kc875.asm.*;
+import kc875.asm.dfa.ASMGraph;
+import kc875.asm.dfa.LiveVariableDFA;
+import kc875.cfg.Graph;
+
+import java.util.*;
+
+public class ASMDeadCodeEliminationVisitor {
+
+    public ASMDeadCodeEliminationVisitor() {
+    }
+
+    private List<ASMInstr> elimDeadCode(List<ASMInstr> func) {
+        ASMGraph graph = new ASMGraph(func);
+        LiveVariableDFA dfa = new LiveVariableDFA(graph);
+        dfa.runWorklistAlgo();
+
+        Map<Graph<ASMInstr>.Node, Set<ASMExprRT>> nodeToLiveVars =
+                dfa.getOutMap();
+
+        List<ASMInstr> optimFunc = new ArrayList<>();
+        for (ASMInstr instr : func) {
+            // If for all defined variables v, v is not live out, skip this
+            // instr
+            Graph<ASMInstr>.Node node = graph.getNode(instr);
+
+            Set<ASMExprRT> allDefs = new HashSet<>(instr.implicitRegs());
+            if (instr.destHasNewDef()) {
+                if (instr instanceof ASMInstr_2Arg) {
+                    allDefs.add((ASMExprRT) ((ASMInstr_2Arg) instr).getDest());
+                } else if (instr instanceof ASMInstr_1Arg) {
+                    allDefs.add((ASMExprRT) ((ASMInstr_1Arg) instr).getArg());
+                }
+            }
+
+            boolean allDefsUseless = allDefs.stream()
+                    .noneMatch(def -> nodeToLiveVars.get(node).contains(def));
+            if (!allDefsUseless)
+                optimFunc.add(instr);
+        }
+
+        return optimFunc;
+    }
+
+    public List<ASMInstr> run(List<ASMInstr> instrs) {
+        return ASMUtils.execPerFunc(instrs, this::elimDeadCode);
+    }
+}
