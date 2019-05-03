955d9ea15297e70fa589db2d04c4a427aa21fee6
anmolkabra <ak2426@cornell.edu>
2019-05-02 20:43:05 -0400

    rax is implicitly defined by SETcc, do that now

--- src/main/java/kc875/asm/ASMInstr_1Arg.java
+++ src/main/java/kc875/asm/ASMInstr_1Arg.java
@@ -90,6 +90,13 @@ public class ASMInstr_1Arg extends ASMInstr {
                 s.add(new ASMExprReg("r10"));
                 s.add(new ASMExprReg("r11"));
                 break;
+            case SETE:
+            case SETNE:
+            case SETG:
+            case SETGE:
+            case SETL:
+            case SETLE:
+                s.add(new ASMExprReg("rax"));
             default:
                 break;
         }
