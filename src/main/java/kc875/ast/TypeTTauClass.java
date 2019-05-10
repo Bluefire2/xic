daafadd97582a36c1cf6d6e1160f203cf3775999
Kirill Chernyshov <kc875@cornell.edu>
2019-05-10 15:59:27 -0400

    Remove superclass information from the class type, since it's now redundant

--- src/main/java/kc875/ast/TypeTTauClass.java
+++ src/main/java/kc875/ast/TypeTTauClass.java
@@ -1,40 +1,20 @@
 package kc875.ast;
 
 import edu.cornell.cs.cs4120.util.CodeWriterSExpPrinter;
-import kc875.utils.Maybe;
 
 public class TypeTTauClass extends TypeTTau {
     private String name;
-    // superClass is unknown if this class is not derived from another class
-    private Maybe<String> superClass;
-
-    public TypeTTauClass(String name, Maybe<String> superClass) {
-        this.name = name;
-        this.superClass = superClass;
-    }
 
     public TypeTTauClass(String name) {
         this.name = name;
-        this.superClass = Maybe.unknown();
-    }
-
-    public TypeTTauClass(String name, String superClass) {
-        this.name = name;
-        this.superClass = Maybe.definitely(superClass);
     }
 
     public String getName() {
         return name;
     }
 
-    public Maybe<String> getSuperClass() {
-        return superClass;
-    }
-
     @Override
     public void prettyPrint(CodeWriterSExpPrinter w) {
-        // TODO: do we need this?
-//        w.printAtom(name + superClass.to(sc -> " extends " + sc.name).otherwise(""));
         w.printAtom(name);
     }
 
@@ -48,9 +28,6 @@ public class TypeTTauClass extends TypeTTau {
 
     @Override
     public String toString() {
-        // TODO: do we need this?
-        // name extends superClass or just name
-//        return name + superClass.to(sc -> " extends " + sc.name).otherwise("");
         return name;
     }
 }
